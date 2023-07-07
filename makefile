b815e34b9d862d0495b6b22a996a6278dbefa15d
syahrul <muhdsyahrulnizam123@gmail.com>
2023-07-07 16:19:44 +0800

    Added http port

--- makefile
+++ makefile
@@ -1,5 +1,6 @@
 build:
 	RUSTFLAGS="-C target-cpu=native" cargo build --release
-start: build
+start:
 	RETH_DB_PATH=~/chain/reth/data/db \
+	HTTP_PORT=9545 \
 	./target/release/reth-server 	
--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,7 @@ async fn main() -> eyre::Result<()> {
         &Path::new(&std::env::var("RETH_DB_PATH")?),
         None,
     )?);
+    let http_port = std::env::var("HTTP_PORT")?;
     let spec = Arc::new(ChainSpecBuilder::mainnet().build());
     let factory = ProviderFactory::new(db.clone(), spec.clone());
 
@@ -74,8 +75,8 @@ async fn main() -> eyre::Result<()> {
     let server = rpc_builder.build(config);
 
     // Start the server & keep it alive
-    let server_args =
-        RpcServerConfig::http(Default::default()).with_http_address("0.0.0.0:8545".parse()?);
+    let server_args = RpcServerConfig::http(Default::default())
+        .with_http_address(format!("0.0.0.0:{}", http_port).parse()?);
     let _handle = server_args.start(server).await?;
     futures::future::pending::<()>().await;
 
