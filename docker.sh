6187b852409b637412f4910baca72758667dc418
syahrul <muhdsyahrulnizam123@gmail.com>
2023-07-13 15:39:25 +0800

    Change script

--- docker.sh
+++ docker.sh
@@ -3,16 +3,26 @@
 # Request user input for the number of services to scale
 read -p "Enter the number of services you want to scale: " scale_number
 
+
+#build images
+docker compose build
+
 # Create the NGINX configuration file
-echo "http {" > nginx.conf
+echo "events {" > nginx.conf
+echo "    worker_connections 1024;" >> nginx.conf
+echo "}" >> nginx.conf
+echo "http {" >> nginx.conf
 echo "    upstream reth-http {" >> nginx.conf
 
 for (( i=0; i<$scale_number; i++ ))
 do
   # Calculate port_http
-  port_http=$((8545 + i * 100))
+  port_http=$((9545 + i * 100))
   # Add server to the NGINX configuration file
   echo "        server localhost:$port_http;" >> nginx.conf
+
+  # Run Docker command for each reth-http service
+  docker run -d --name reth-http-$i -p $port_http:8545 -v ~/chain/reth/data/db:/data/db -e RETH_DB_PATH=/data/db reth-http
 done
 
 echo "    }" >> nginx.conf  # Close reth-http upstream
@@ -22,22 +32,25 @@ echo "    upstream reth-ws {" >> nginx.conf
 for (( i=0; i<$scale_number; i++ ))
 do
   # Calculate port_ws
-  port_ws=$((8546 + i * 100))
+  port_ws=$((9546 + i * 100))
   # Add server to the NGINX configuration file
   echo "        server localhost:$port_ws;" >> nginx.conf
+
+  # Run Docker command for each reth-ws service
+  docker run -d --name reth-ws-$i -p $port_ws:8546 -v ~/chain/reth/data/db:/data/db -e RETH_DB_PATH=/data/db reth-ws
 done
 
 echo "    }" >> nginx.conf  # Close reth-ws upstream
 
 # Continue with the server blocks and close the http block
 echo "    server {" >> nginx.conf
-echo "        listen 8080;" >> nginx.conf
+echo "        listen 8545;" >> nginx.conf
 echo "        location / {" >> nginx.conf
 echo "            proxy_pass http://reth-http;" >> nginx.conf
 echo "        }" >> nginx.conf
 echo "    }" >> nginx.conf
 echo "    server {" >> nginx.conf
-echo "        listen 8081;" >> nginx.conf
+echo "        listen 8546;" >> nginx.conf
 echo "        location / {" >> nginx.conf
 echo "            proxy_pass http://reth-ws;" >> nginx.conf
 echo "            proxy_http_version 1.1;" >> nginx.conf
@@ -46,14 +59,3 @@ echo "            proxy_set_header Connection \"Upgrade\";" >> nginx.conf
 echo "        }" >> nginx.conf
 echo "    }" >> nginx.conf
 echo "}" >> nginx.conf
-
-# Run Docker Compose command for each service
-for (( i=0; i<$scale_number; i++ ))
-do
-  # Calculate ports
-  port_http=$((9545 + i * 100))
-  port_ws=$((9546 + i * 100))
-
-  # Run Docker Compose command
-  INDEX=$i PORT=$port_http PORT_WS=$port_ws docker compose up -d --scale reth-http=$scale_number --scale reth-ws=$scale_number --scale nginx=1
-done
